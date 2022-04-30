47acac67501fdb578ee4b8e63fd6c6e96dd5e609
sawdahoque234 <sawdahoque6066@gmail.com>
2022-04-30 23:13:27 +0600

    review-card-template

---
+++ src/Reviews/Review.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import reviews from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { name, position, image, text } = reviews[index];
+
+  
+  const checkNumber = (number) => {
+    if (number > reviews.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return reviews.length - 1;
+    }
+    return number;
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+ 
+  return (
+    <main>
+        <section className='container'>
+        <div className='title'>
+        <h2 >Our Testimonial</h2>
+        </div>
+        <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{position}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+     
+    </article>
+    </section>
+    </main>
+  );
+
+};
+
+export default Review;
